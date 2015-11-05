1a9616d433dc0162ea3fa09b623d0b6217c1f3e1
alu0100502107 <alu0100502107@ull.edu.es>
2015-11-05 19:14:34 +0000

    Añadir expectativa fecha de publicación
    Añadida nueva expectativa fecha de publicación

--- spec/bibliografia_spec.rb
+++ spec/bibliografia_spec.rb
@@ -8,7 +8,8 @@ describe Bibliografia do
         "Programming Ruby 1.9 & 2.0: The Pragmatic Programmers' Guide",
         "(The Facets of Ruby)",
 	    "Pragmatic Bookshelf",
-	    "4 edition"
+	    "4 edition",
+	    "(July 7, 2013)",
 	)
    end
    
@@ -30,6 +31,10 @@ describe Bibliografia do
 
    it "debe existir un número de edición" do
      expect(@libro.num_edicion).not_to be_nil
+   end
+   
+   it "debe existir una fecha de publicación" do
+     expect(@libro.fecha_publicacion).not_to be_nil
    end
    
 end
