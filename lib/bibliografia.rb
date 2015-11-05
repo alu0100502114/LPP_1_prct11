51758b48a29c2ff4c32ddd7e20dde6f08b82d2d2
Jesús Marín <alu0100502114@ull.edu.es>
2015-11-05 19:26:20 +0000

    Añadir print_isbn a la clase
    Añadida propiedad nueva

--- lib/bibliografia.rb
+++ lib/bibliografia.rb
@@ -6,16 +6,17 @@ module Bibliografia
   # Clase Referencia para gestionar las de una Bibliografía
   class Referencia
     # Getters + Setters
-    attr_accessor :autores, :titulo, :serie, :editorial, :num_edicion, :fecha_publicacion
+    attr_accessor :autores, :titulo, :serie, :editorial, :num_edicion, :fecha_publicacion, :num_isbns
 
     # Constructor
-    def initialize(autores, titulo, serie, editorial, num_edicion, fecha_publicacion)
+    def initialize(autores, titulo, serie, editorial, num_edicion, fecha_publicacion, num_isbns)
       @autores = autores
       @titulo = titulo
       @serie = serie
       @editorial = editorial
       @num_edicion = num_edicion
       @fecha_publicacion = fecha_publicacion
+      @num_isbns = num_isbns
     end
 
     # Muestra un autor de la lista
@@ -29,12 +30,24 @@ module Bibliografia
       lista
     end
 
+    # Muestra lista de isbn
+    def print_isbn
+      count = 0
+      lista = ""
+      isbns.each do |isbn|
+        lista += "ISBN-" + isbn.size + ": " + isbn
+        lista += "\n" if ++count != isbns.size
+      end
+      lista
+    end
+
     # Para método puts
     def to_s
       "#{print_autor}\n
        #{titulo}\n
        #{serie}\n
        #{editorial}; #{num_edicion} (#{fecha_publicacion})\n
+       #{print_isbn}\n
       "
     end
   end
