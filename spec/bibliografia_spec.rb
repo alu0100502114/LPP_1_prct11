76655e373e571a62c47179932ec36de142c49566
alu0100502107 <alu0100502107@ull.edu.es>
2015-11-05 20:43:53 +0000

    Añadir expectativa listado ISBN
    Añadida expectativa nueva para obtener el listado de ISBN

--- spec/bibliografia_spec.rb
+++ spec/bibliografia_spec.rb
@@ -10,7 +10,7 @@ describe Bibliografia do
 	    "Pragmatic Bookshelf",
 	    "4 edition",
 	    "(July 7, 2013)",
-	    ["968-81937785499", "1937785491"]
+	    [" 968-1937785499", "1937785491"]
 	)
    end
    
@@ -69,6 +69,5 @@ describe Bibliografia do
    it "Existe un método para obtener el listado ISBN" do
      expect(@libro.print_isbn).to eq(["ISBN-13: 968-1937785499\nISBN-10: 1937785491"])
    end
-   
    
 end   
