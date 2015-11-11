bfdd52c822e28ba56f075edc76cc181572c7f779
Jesús Marín <alu0100502114@ull.edu.es>
2015-11-11 21:53:04 +0000

    Implementar lista
    Primera implementación de una lista

---
+++ lib/bibliografia/lista_impl.rb
@@ -0,0 +1,9 @@
+# encoding: utf-8
+#
+# Nodo para Lista
+# con "value" para valor del Nodo
+#     "next" para puntero a siguiente nodo
+#     "prev" para punteo a anterior nodo
+Node = Struct.new(:value, :next, :prev) do
+
+end
