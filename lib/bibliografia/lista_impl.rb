8e78435d46bf98e034b0324c060298b0c7a767a3
Jesús Marín <alu0100502114@ull.edu.es>
2015-11-20 15:19:19 +0000

    Cambios finales

--- lib/bibliografia/lista_impl.rb
+++ lib/bibliografia/lista_impl.rb
@@ -3,7 +3,7 @@
 # Nodo para Lista
 # con "value" para valor del Nodo
 #     "next" para puntero a siguiente nodo
-#     "prev" para puntero a anterior nodo
+#     "prev" para punter;o a anterior nodo
 Node = Struct.new(:value, :next, :prev) do
   
   # Mostrar por pantalla un nodo
