6eb7c93577f3b9970fee0640d3f6cbcc889c6f10
Jesús Marín <alu0100502114@ull.edu.es>
2015-12-11 21:28:32 +0000

    intentando que travis funcione

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'bibliografia'
 require 'coveralls'
 Coveralls.wear!
+
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'bibliografia'
